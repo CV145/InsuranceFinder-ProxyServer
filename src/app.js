e869f09d962e5308029f5f8defbfd6aa8805d14c
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2024-01-21 19:11:16 -0600

    test cors

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const apiProxyRoutes = require('./routes/apiProxyRoutes');
 const app = express();
 
 // Enable CORS for your frontend origin
-app.use(cors({
+/*app.use(cors({
     origin: 'http://localhost:5173' // Replace with your frontend's origin
-}));
+}));*/
+
+app.use(cors());
+
 
 app.use(express.json());
 app.use('/api', apiProxyRoutes);
