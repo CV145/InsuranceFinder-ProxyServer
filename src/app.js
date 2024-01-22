80c24354381b08e28a04c5ca04b1b73c0ad54fa6
Carlos Valeriano <carlosvaleriano45@yahoo.com>
2024-01-22 09:14:43 -0600

    CORS custom middleware

--- src/app.js
+++ src/app.js
@@ -5,19 +5,23 @@ import apiProxyRoutes from './routes/apiProxyRoutes.js';
 
 const app = express();
 
-// Enable CORS for multiple origins
-const allowedOrigins = ['https://cv145.github.io', 'http://localhost:5173'];
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
+// Custom CORS middleware
+const customCors = (req, res, next) => {
+    const allowedOrigins = ['https://cv145.github.io', 'http://localhost:5173'];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
     }
-}));
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+};
+
+app.use(customCors);
 
 app.use(express.json());
 app.use('/api', apiProxyRoutes);
